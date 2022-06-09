037b49c3959d846de85b2a14eb631973a27965ac
Carol Nagata <86692269+CarolNagata@users.noreply.github.com>
2022-06-09 17:13:34 -0300

    Add files via upload

---
+++ ifoodProject/ifoodproject/src/main/java/com/ifoodacademicproject/ifoodproject/IfoodprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ifoodacademicproject.ifoodproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfoodprojectApplication.class, args);
+	}
+
+}
---
+++ ifoodProject/ifoodproject/src/test/java/com/ifoodacademicproject/ifoodproject/IfoodprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifoodacademicproject.ifoodproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoodprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
